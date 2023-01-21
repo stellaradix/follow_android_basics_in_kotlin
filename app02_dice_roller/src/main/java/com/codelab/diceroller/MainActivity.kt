c5f1ed290cd5d9905dbb280d683eb30c8f6898b3
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-01-21 11:57:54 +0900

    App02: Interactive - onClick handler
    findViewById(R.id.~) in Activity
    - return view object with matching id in Activity
    - automatic type cast to assigned variable
    
    {view object}.setText
    - set text property value of view
    
    {view object}.setOnClickListener
    - register click event handler to view object
    
    Toast.makeText(Context, String, Toast.LENGTH_..).show()
    - display message
    
    Attribute of TextView
    - tools:text
    
    === === ===
    Course: Android Basics in Kotlin
    Unit 1: Kotlin basics
    Path 4: Add a button to an app

--- app02_dice_roller/src/main/java/com/codelab/diceroller/MainActivity.kt
+++ app02_dice_roller/src/main/java/com/codelab/diceroller/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.codelab.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +11,14 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		val rollButton: Button = findViewById(R.id.roll_button)
+
+		rollButton.setOnClickListener {
+			Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+
+			val resultTextView: TextView = findViewById(R.id.dice_text)
+			resultTextView.text = "6"
+		}
 	}
 }
