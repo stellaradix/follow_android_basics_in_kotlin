b29867eeb4008bd21a2b06c7c075137bb003b724
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-01-26 00:19:50 +0900

    App08: Add liveData and observer
    LiveData<>
    - data holder that can be observed within a given lifecycle
    
    MutableLiveData<>
    - mutable version of LiveData
    
    livedata.observe(viewLifecycleOwner, lambda)
    - attach observer to livedata
    
    === === ===
    Course: Android Basics in Kotlin
    Unit 3: Navigation
    Path 3: Architecture components

--- app08_unscramble/src/main/java/com/codelab/unscramble/ui/game/GameFragment.kt
+++ app08_unscramble/src/main/java/com/codelab/unscramble/ui/game/GameFragment.kt
@@ -50,10 +50,20 @@ class GameFragment : Fragment() {
 		binding.skip.setOnClickListener { onSkipWord() }
 		// Update the UI
 		updateNextWordOnScreen()
-		binding.score.text = getString(R.string.score, 0)
-		binding.wordCount.text = getString(
-			R.string.word_count, 0, MAX_NO_OF_WORDS
-		)
+
+		// Observe the currentScrambledWord LiveData.
+		viewModel.currentScrambledWord.observe(viewLifecycleOwner) { newWord ->
+			binding.textViewUnscrambledWord.text = newWord
+		}
+
+		viewModel.score.observe(viewLifecycleOwner) { newScore ->
+			binding.score.text = getString(R.string.score, newScore)
+		}
+
+		viewModel.currentWordCount.observe(viewLifecycleOwner) { newWordCount ->
+			binding.wordCount.text =
+				getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
+		}
 	}
 
 	/*
@@ -95,7 +105,7 @@ class GameFragment : Fragment() {
 	private fun showFinalScoreDialog() {
 		MaterialAlertDialogBuilder(requireContext())
 			.setTitle(getString(R.string.congratulations))
-			.setMessage(getString(R.string.you_scored, viewModel.score))
+			.setMessage(getString(R.string.you_scored, viewModel.score.value))
 			.setCancelable(false)
 			.setNegativeButton(getString(R.string.exit)) { _, _ ->
 				exitGame()
@@ -145,6 +155,6 @@ class GameFragment : Fragment() {
 	 * Displays the next scrambled word on screen.
 	 */
 	private fun updateNextWordOnScreen() {
-		binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
+		binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord.value
 	}
 }
--- app08_unscramble/src/main/java/com/codelab/unscramble/ui/game/GameViewModel.kt
+++ app08_unscramble/src/main/java/com/codelab/unscramble/ui/game/GameViewModel.kt
@@ -1,18 +1,20 @@
 package com.codelab.unscramble.ui.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-	private var _score = 0
-	val score get() = _score
+	private val _score = MutableLiveData(0)
+	val score: LiveData<Int> get() = _score
 
-	private var _currentWordCount = 0
-	val currentWordCount get() = _currentWordCount
+	private val _currentWordCount = MutableLiveData(0)
+	val currentWordCount: LiveData<Int> get() = _currentWordCount
 
-	private lateinit var _currentScrambledWord: String
-	val currentScrambledWord get() = _currentScrambledWord
+	private val _currentScrambledWord = MutableLiveData<String>()
+	val currentScrambledWord: LiveData<String> get() = _currentScrambledWord
 
 	private var wordsList: MutableList<String> = mutableListOf()
 	private lateinit var currentWord: String
@@ -43,8 +45,8 @@ class GameViewModel : ViewModel() {
 		if (wordsList.contains(currentWord)) {
 			getNextWord()
 		} else {
-			_currentScrambledWord = String(tempWord)
-			++_currentWordCount
+			_currentScrambledWord.value = String(tempWord)
+			_currentWordCount.value = (_currentWordCount.value)?.inc()
 			wordsList.add(currentWord)
 		}
 	}
@@ -53,14 +55,14 @@ class GameViewModel : ViewModel() {
 	* Re-initializes the game data to restart the game.
 	*/
 	fun reinitializeData() {
-		_score = 0
-		_currentWordCount = 0
+		_score.value = 0
+		_currentWordCount.value = 0
 		wordsList.clear()
 		getNextWord()
 	}
 
 	private fun increaseScore() {
-		_score += SCORE_INCREASE
+		_score.value = (_score.value)?.plus(SCORE_INCREASE)
 	}
 
 	fun isUserWordCorrect(playerWord: String): Boolean {
@@ -76,7 +78,7 @@ class GameViewModel : ViewModel() {
 	* Updates the next word.
 	*/
 	fun nextWord(): Boolean {
-		return if (currentWordCount < MAX_NO_OF_WORDS) {
+		return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
 			getNextWord()
 			true
 		} else false
