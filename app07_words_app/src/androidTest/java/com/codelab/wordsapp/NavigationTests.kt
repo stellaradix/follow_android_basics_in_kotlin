7b6c2e243d10f10791590383150ab56994c1b78c
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-01-25 22:50:24 +0900

    App07: Update test to use before annotation (custom)
    === === ===
    Course: Android Basics in Kotlin
    Unit 3: Navigation
    Path 2: Introduction to the Navigation component

--- app07_words_app/src/androidTest/java/com/codelab/wordsapp/NavigationTests.kt
+++ app07_words_app/src/androidTest/java/com/codelab/wordsapp/NavigationTests.kt
@@ -1,5 +1,6 @@
 package com.codelab.wordsapp
 
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
@@ -11,30 +12,49 @@ import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class NavigationTests {
 
-	@Test
-	fun navigate_to_words_nav_component() {
-		val navController = TestNavHostController(
-			ApplicationProvider.getApplicationContext()
-		)
+	private lateinit var navController: TestNavHostController
 
-		val letterListScenario =
-			launchFragmentInContainer<LetterListFragment>(themeResId = R.style.Theme_WordsApp)
+	private lateinit var letterListScenario: FragmentScenario<LetterListFragment>
+
+	@Before
+	fun setup() {
+		navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+
+		letterListScenario = launchFragmentInContainer(themeResId = R.style.Theme_WordsApp)
 
 		letterListScenario.onFragment { fragment ->
 			navController.setGraph(R.navigation.nav_graph)
 			Navigation.setViewNavController(fragment.requireView(), navController)
 		}
+	}
 
+	@Test
+	fun navigate_to_words_nav_component_0() {
+		navigate_to_words_nav_component(0)
+	}
+
+	@Test
+	fun navigate_to_words_nav_component_1() {
+		navigate_to_words_nav_component(1)
+	}
+
+	@Test
+	fun navigate_to_words_nav_component_2() {
+		navigate_to_words_nav_component(2)
+	}
+
+	private fun navigate_to_words_nav_component(position: Int) {
 		onView(withId(R.id.recycler_view))
 			.perform(
 				RecyclerViewActions
-					.actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click())
+					.actionOnItemAtPosition<RecyclerView.ViewHolder>(position, click())
 			)
 
 		assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
