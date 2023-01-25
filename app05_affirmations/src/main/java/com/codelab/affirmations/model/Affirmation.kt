5ca3283857a1d7d76dda979f5c094574ab1014de
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-01-25 14:36:56 +0900

    App05: Add image source to RecyclerView
    To set data onto View
    - Modify data model class
    - Modify data source
    - Update list item layout
    - Updata adapter
    
    Resource Annotation
    - ensure right type of Resource ID
    
    === === ===
    Course: Android Basics in Kotlin
    Unit 2: Layouts
    Path 3: Display a scrollable list

--- app05_affirmations/src/main/java/com/codelab/affirmations/model/Affirmation.kt
+++ app05_affirmations/src/main/java/com/codelab/affirmations/model/Affirmation.kt
@@ -1,5 +1,9 @@
 package com.codelab.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 class Affirmation(
-	val stringResourceId: Int
+	@StringRes val stringResourceId: Int,
+	@DrawableRes val imageResourceId: Int
 )
