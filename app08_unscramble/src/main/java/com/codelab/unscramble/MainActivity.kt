4b8b0029e1f7628e376b5f3b962217a3693a15a9
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-01-25 23:33:28 +0900

    App08: Import start code
    === === ===
    Course: Android Basics in Kotlin
    Unit 3: Navigation
    Path 3: Architecture components

--- app08_unscramble/src/main/java/com/codelab/unscramble/MainActivity.kt
+++ app08_unscramble/src/main/java/com/codelab/unscramble/MainActivity.kt
@@ -3,6 +3,9 @@ package com.codelab.unscramble
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * Creates an Activity that hosts the Game fragment in the app
+ */
 class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
