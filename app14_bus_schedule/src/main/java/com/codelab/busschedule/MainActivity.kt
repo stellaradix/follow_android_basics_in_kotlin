888dcb3d0324ffdec2209d863bbb8ead410ac315
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-01-27 01:55:47 +0900

    App14: Import start code
    === === ===
    Course: Android Basics in Kotlin
    Unit 5: Data persistence
    Path 1: Introduction to SQL, Room, and Flow

--- app14_bus_schedule/src/main/java/com/codelab/busschedule/MainActivity.kt
+++ app14_bus_schedule/src/main/java/com/codelab/busschedule/MainActivity.kt
@@ -2,11 +2,28 @@ package com.codelab.busschedule
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.codelab.busschedule.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+	private lateinit var navController: NavController
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+
+		val binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+
+		val navHostFragment = supportFragmentManager
+			.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+		navController = navHostFragment.navController
+		setupActionBarWithNavController(navController)
+	}
+
+	override fun onSupportNavigateUp(): Boolean {
+		return navController.navigateUp() || super.onSupportNavigateUp()
 	}
 }
